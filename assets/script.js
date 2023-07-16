4789ad2f87a421a7b3e5550c64c7c5402acf890f
Ashley Zemina <amzemina@gmail.com>
2023-07-16 11:58:32 -0400

    added function that doesnt duplicate cities in local storage

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,39 @@
 var apiKey = "3bdc417e78dfd8db03774da5b237d55a";
 
 $(document).ready(function () {
-//search city
+
+// todo: fix comment
+    //search city
 function getCities() {
     var cities = localStorage.getItem("cities")
      if (!cities) {
-        
         return []
      } 
-     
      return JSON.parse(cities)
 }
+
+// todo: fix comment
 //event listener
+// todo: format
 $("#search-btn").on("click", function(event) {
 event.preventDefault()
 
 var searchBox = $("#search-input")
-
-    localStorage.setItem("cities", JSON.stringify($.merge(getCities(), [searchBox.val()])))
-   
+    // todo: call api for city as a new function. if not valid, don't add to list and alert user. if valid, add to list and display
+    
+    if (!getCities().includes(searchBox.val())) {
+        
+        localStorage.setItem("cities", JSON.stringify([...getCities(), searchBox.val()]));
+    }
+    // todo: display the cities on html through a new function
 });
+
+
+
 })
+
+
+
 //Current weather
 // function currentWeather()
 
